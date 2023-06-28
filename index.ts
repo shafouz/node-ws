6bc1fb3017917307eacae8bf06aa4db0d2dcc010
shafou <jlmacedomatos@gmail.com>
2023-06-28 14:44:41 -0400

    need to fix room ids

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import { readFileSync } from "fs";
 import * as http from "http";
-import { startWebSocketServer, Room } from "./src/ws";
+import { startWebSocketServer, Room, printRooms } from "./src/ws";
 import { render } from "ejs";
 import moment from "moment";
 
@@ -15,6 +15,23 @@ console.log(`Started ws server on port ${8001}`);
 
 function message_template(name: string, message: string, timestamp: string) {
   return `${timestamp} - ${name}: ${message}`;
+}
+
+function fmt_users(room: Room | undefined): string[] {
+  console.log("DEBUGPRINT[12]: index.ts:21: room=", room);
+
+  if (room === undefined) {
+    return [];
+  }
+
+  let users: string[] = [];
+
+  for (const user of room.users) {
+    users.push(user.name);
+  }
+
+  console.log("DEBUGPRINT[11]: index.ts:31: users=", users);
+  return users;
 }
 
 function fmt_messages(room: Room | undefined): string[] {
@@ -43,23 +60,25 @@ function fmt_messages(room: Room | undefined): string[] {
 }
 
 const server = http.createServer((req, res) => {
-  // get query for room
-  // render room messages on first load?
   let url = new URL(req.url ? req.url : host, `http://${req.headers.host}`);
 
-  let roomId = parseInt(url.searchParams.get("room") ?? "0");
-  let room = rooms.find((room) => room.id === roomId);
+  if (url.pathname === "/src/frontend") {
+    res.writeHead(200, { "Content-Type": "application/javascript" });
+    res.end(readFileSync("./src/frontend.js"));
+  } else {
+    let roomId = parseInt(url.searchParams.get("room") ?? "0");
+    let room = rooms.find((room) => room.id === roomId);
 
-  let page = readFileSync("./index.html");
+    let page = readFileSync("./index.html");
+    console.log("DEBUGPRINT[15]: index.ts:73: rooms=", rooms);
 
-  // console.log("DEBUGPRINT[3]: index.ts:25: rooms=", rooms);
-  // console.log("DEBUGPRINT[2]: index.ts:25: room=", room);
+    let messages = fmt_messages(room);
+    let users = fmt_users(room);
+    let html = render(page.toString(), { messages, users });
 
-  let messages = fmt_messages(room);
-  let html = render(page.toString(), { messages });
-
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.end(html);
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(html);
+  }
 });
 
 console.log(`Started on port ${port}`);
--- src/ws.ts
+++ src/ws.ts
@@ -9,7 +9,12 @@ export type Room = {
 type User = {
   name: string;
   messages: Message[];
-  connection: WebSocket;
+  connection: WebSocketWithUsername;
+};
+
+type WebSocketWithUsername = WebSocket & {
+  username: string;
+  room: number;
 };
 
 type Message = {
@@ -17,84 +22,127 @@ type Message = {
   timestamp: string;
 };
 
-// on connect create a new room
-// add user to room
+function printRooms(rooms: Room[]) {
+  let pp = rooms.map((room) =>
+    room.users.map((user) => {
+      return { user: user.name, messages: user.messages };
+    })
+  );
+  console.log(JSON.stringify(pp));
+}
+
+function get_room_and_user(
+  parsed: any,
+  rooms: Room[],
+  ws: WebSocketWithUsername
+): { user: User; room: Room } {
+  let user: User = {
+    name: "",
+    messages: [],
+    connection: ws,
+  };
+
+  let roomId = parsed.room;
+  let room = rooms.find((room) => room.id === roomId);
+
+  if (room) {
+    // room case
+    // check if user is not in room
+    // add user to room
+    let user_check = room.users.find((user) => user.name === parsed.user);
+
+    if (!user_check) {
+      // create user
+      user = {
+        name: parsed.user,
+        messages: [],
+        connection: ws,
+      };
+
+      // add user to room
+      room.users.push(user);
+    }
+  } else {
+    // no room case
+    // create new room
+    // add user to room
+    // add room to rooms
+    room = {
+      id: roomId,
+      users: [],
+    };
+
+    user = {
+      name: parsed.user,
+      messages: [],
+      connection: ws,
+    };
+
+    room.users.push(user);
+    rooms.push(room);
+  }
+
+  return { user, room };
+}
+
 function startWebSocketServer(rooms: Room[]): WebSocketServer {
   const wss = new WebSocketServer({ port: 8001 });
 
-  wss.on("connection", function connection(ws) {
-    ws.on("error", console.error);
+  wss.on("connection", function connection(ws: WebSocketWithUsername) {
+    const pingInterval = 2500; // Interval in milliseconds (e.g., 5 seconds)
+
+    const pingIntervalId = setInterval(() => {
+      if (ws.readyState === WebSocket.OPEN) {
+        // console.log("ping");
+        ws.ping();
+      }
+    }, pingInterval);
+
+    ws.on("pong", () => {
+      // console.log("pong");
+    });
+
+    ws.on("close", () => {
+      clearInterval(pingIntervalId);
+      ws.terminate();
+
+      let room = rooms.find((room) => room.id === ws.room);
+
+      room?.users.splice(
+        room?.users.findIndex((user) => user.name === ws.username),
+        1
+      );
+    });
 
     ws.on("message", function message(data) {
       let parsed = JSON.parse(data.toString());
-      let roomId = parseInt(parsed.room);
-      roomId = isNaN(roomId) ? 0 : roomId;
 
-      let room = rooms.find((room) => room.id === roomId);
-      let user: User | undefined;
-
-      if (room) {
-        // room case
-        // check if user is not in room
-        // add user to room
-        user = room.users.find((user) => user.name === parsed.user);
-
-        if (!user) {
-          // create user
-          user = {
-            name: parsed.user,
-            messages: [],
-            connection: ws,
-          };
-
-          // add user to room
-          room.users.push(user);
-        }
-      } else {
-        // no room case
-        // create new room
-        // add user to room
-        // add room to rooms
-        room = {
-          id: roomId,
-          users: [],
-        };
-
-        user = {
-          name: parsed.user,
-          messages: [],
-          connection: ws,
-        };
-
-        room.users.push(user);
-        rooms.push(room);
-      }
+      let { room, user } = get_room_and_user(parsed, rooms, ws);
+      console.log("DEBUGPRINT[13]: ws.ts:121: room=", room);
+      console.log("DEBUGPRINT[14]: ws.ts:122: user=", user.name);
 
       parsed.timestamp = moment(Date.now()).format("HH:mm:ss");
-
-      let message: Message = {
+      let message = {
         message: parsed.message,
         timestamp: parsed.timestamp,
       };
-
       user?.messages.push(message);
 
-      room.users.forEach((__user) => {
+      room?.users.forEach((__user) => {
         if (__user.connection.readyState === WebSocket.OPEN) {
-          // let pp = rooms.map((room) =>
-          //   room.users.map((user) => {
-          //     return { user: user.name, messages: user.messages };
-          //   })
-          // );
-          // console.log(JSON.stringify(pp));
+          // printRooms(rooms);
 
           __user.connection.send(JSON.stringify(parsed));
         }
       });
+
+      // bad
+      ws.username = user.name;
+      ws.room = room.id;
     });
   });
 
   return wss;
 }
 
-export { startWebSocketServer };
+export { startWebSocketServer, printRooms };
