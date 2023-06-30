7229396f5910a606f4326325f60a6f06d39034f3
shafou <jlmacedomatos@gmail.com>
2023-06-30 09:53:09 -0400

    mor tets

--- tests/ws.test.js
+++ tests/ws.test.js
@@ -52,3 +52,62 @@ describe("basic functionality", () => {
     expect(users_page_1.length).toEqual(2);
   });
 });
+
+describe("rooms work", () => {
+  beforeAll(async () => {
+    await page.goto("http://localhost:8000/?room=1");
+
+    const form = await page.$("#message");
+    await form.type("this is room 1");
+
+    const button = await page.$("#sendMessage");
+    await button.click();
+  });
+
+  it("send message", async () => {
+    const obj = await page.evaluate(() => {
+      const user = document.querySelector("#users");
+      const messages = document.querySelector("#messages");
+
+      return {
+        user: user.children[0].textContent,
+        user_len: user.children.length,
+        message: messages.children[0].textContent,
+        messages_len: messages.children.length,
+      };
+    });
+
+    expect(obj.user.length).toEqual(16);
+    expect(obj.user_len).toEqual(1);
+    expect(obj.message.includes("this is room 1")).toEqual(true);
+    expect(obj.messages_len).toEqual(1);
+
+    const incognito = await browser.createIncognitoBrowserContext();
+    const page2 = await incognito.newPage();
+    await page2.goto("http://localhost:8000/?room=1");
+    const form2 = await page2.$("#message");
+    await form2.type("this is room 2");
+    const button2 = await page2.$("#sendMessage");
+    await button2.click();
+
+    const obj2 = await page2.evaluate(() => {
+      const user = document.querySelector("#users");
+      const messages = document.querySelector("#messages");
+
+      return {
+        user_len: user.children.length,
+        messages_len: messages.children.length,
+      };
+    });
+
+    expect(obj2.user_len).toEqual(2);
+    expect(obj2.messages_len).toEqual(2);
+
+    const users_page_1 = await page.$$("#users > b");
+    expect(users_page_1.length).toEqual(2);
+
+    await page2.goto("http://localhost:8000/?room=10");
+    const users_page_room_0 = await page2.$$("#users > b");
+    expect(users_page_room_0.length).toEqual(1);
+  });
+});
